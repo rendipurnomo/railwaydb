6677c937f3bc44c1528f4612075f2b1928f18059
Rendi Purnomo <107144802+rendipurnomo@users.noreply.github.com>
2024-03-28 06:23:02 +0700

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const userController = require("../controllers/user.controller.js");
 const { protect, protectAdmin } = require("../middleware/protect.js");
 
-router.get("/",protect , userController.getUsers);
-router.get("/:id", protect, userController.getUserById);
-router.put("/:id", protect, userController.updateUser);
-router.delete("/:id", protect, protectAdmin, userController.deleteUser);
-router.patch("/roles/:id", protect, protectAdmin, userController.updateRolesUser);
-router.patch("/event/:id", protect, protectAdmin, userController.updateEventUser);
-router.patch("/resetEvent", protect, protectAdmin, userController.resetEventUser);
+router.get("/", userController.getUsers);
+router.get("/:id", userController.getUserById);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+router.patch("/roles/:id", userController.updateRolesUser);
+router.patch("/event/:id", userController.updateEventUser);
+router.patch("/resetEvent", userController.resetEventUser);
 
 module.exports = router
